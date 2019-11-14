af32a6eab35d86be2f6178b94ef873569277070c
mengqing <54385579+xiamengqing@users.noreply.github.com>
2019-11-14 17:39:01 +0800

    Add files via upload

---
+++ a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+        int a = 10;
+	int b = 2;
+	int c = 0;
+	
+	scanf("%d",&c);
+	printf("%d\n",a*b*c);
+	printf("Hello World!\n");
+	return 0;
+}
